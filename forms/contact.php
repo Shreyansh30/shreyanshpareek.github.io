9222c94df03f664fc1301a11f00d49230956d8f8
Shreyansh30 <shreyanshpareek3025@gmail.com>
2021-04-07 12:31:52 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,21 +7,21 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  // $receiving_email_address = 'contact@example.com';
+   $receiving_email_address = 'contact@example.com';
 
-  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-  //   include( $php_email_form );
-  // } else {
-  //   die( 'Unable to load the "PHP Email Form" Library!');
-  // }
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
 
-  // $contact = new PHP_Email_Form;
-  // $contact->ajax = true;
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
   
-  // $contact->to = $receiving_email_address;
-  // $contact->from_name = $_POST['name'];
-  // $contact->from_email = $_POST['email'];
-  // $contact->subject = $_POST['subject'];
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -33,31 +33,31 @@
   );
   */
 
-  // $contact->add_message( $_POST['name'], 'From');
-  // $contact->add_message( $_POST['email'], 'Email');
-  // $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
 
-  // echo $contact->send();
-
-
+  echo $contact->send();
 
 
 
 
 
-if (isset($_POST['submit'])) {
-  $name = $_POST['name'];
-  $subject = $_POST['subject'];
-  $mailFrom = $_POST['mail'];
-  $message = $_POST['message'];
 
-  $mailTo = "shreyanshpareek3025@gmail.com";
-  $headers = "From: ".$mailFrom;
-  $txt = "YOu have received an e-mail from ".$name.".\n\n".$message;
 
-  mail($mailTo, subject,$txt, $headers);
-  header("Location: index.php?mailsend");
-  # code...
-}
+// if (isset($_POST['submit'])) {
+//   $name = $_POST['name'];
+//   $subject = $_POST['subject'];
+//   $mailFrom = $_POST['mail'];
+//   $message = $_POST['message'];
+
+//   $mailTo = "shreyanshpareek3025@gmail.com";
+//   $headers = "From: ".$mailFrom;
+//   $txt = "YOu have received an e-mail from ".$name.".\n\n".$message;
+
+//   mail($mailTo, subject,$txt, $headers);
+//   header("Location: index.php?mailsend");
+//   # code...
+// }
 
 ?>
